075c9426bcdf319aeb689c1a8e34280d4efab662
Trung Nguyen <43191329+kylenguyen1014@users.noreply.github.com>
2019-07-30 19:55:01 -0400

    style Board and Jokes component

--- src/Board.js
+++ src/Board.js
@@ -2,36 +2,109 @@ import React, { Component } from 'react';
 import logo from './main-icon.png';
 import './Board.css';
 import axios from 'axios';
+import Jokes from './Jokes';
 
 class Board extends Component {
     constructor(props) {
         super(props);
-        this.state = { jokes:[] };
+        this.state = { 
+            jokes:[],
+            isLoaded: false 
+        };
         this.newJokes = this.newJokes.bind(this);
+        this.upVote = this.upVote.bind(this);
+        this.downVote = this.downVote.bind(this);
     }
 
     async componentDidMount() {
-        this.newJokes();
+        setTimeout(() => {
+            this.newJokes();
+            this.setState({isLoaded: true});
+        }, 3000);
+    }
+
+
+
+    componentDidUpdate(prevProps, prevState) {
+        
     }
     
 
     async newJokes(){
         for (let i=0; i < 10;i++){
-            let res = await axios.get('https://icanhazdadjoke.com/', {headers : {Accept :"application/json"}})
-            console.log(res.data);
+            let res;
+            do {
+                res = await axios.get('https://icanhazdadjoke.com/', {headers : {Accept :"application/json"}})
+            } while (this.checkDuplicateJoke(res.data.id))
+   
+            this.setState(st => ({
+                jokes : [...st.jokes, {id : res.data.id, joke: res.data.joke, vote : 0}]
+            }))
         }
     }
-    render() { 
-        return ( 
-            <div className='Board'>
-                <div>
-                    <h1>Dad <span>Joke</span></h1>
-                    <img className='Board-logo' alt='Main-icon' src={logo}/>
-                    <button onClick={this.newJokes}>New Jokes</button>
-                </div>
-                <div>
 
-                </div>
+    checkDuplicateJoke(id){
+        if (this.state.jokes.some(joke => joke.id === id)){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    compareVote(a,b){
+        const vote1 = a.vote;
+        const vote2 = b.vote;
+        return (vote1 - vote2) * -1;
+    }
+
+    upVote(id){
+        const array = this.state.jokes.map(joke => {
+            if (id === joke.id){
+                return {...joke, vote : joke.vote + 1}
+            }
+            return joke;
+        });
+
+        this.setState({jokes : array.sort(this.compareVote)});
+    }
+
+    downVote(id){
+        const array = this.state.jokes.map(joke => {
+            if (id === joke.id){
+                return {...joke, vote : joke.vote - 1}
+            }
+            return joke;
+        });
+    
+        this.setState({jokes : array.sort(this.compareVote)});
+    }
+    render() { 
+        return (
+            <div className='Board'>
+                {this.state.isLoaded 
+                ?
+                    (<div className='Board-1'>
+                        <div className='Board-main'>
+                            <h1 className='Board-main-title'>Dad <span>Jokes</span></h1>
+                            <img className='Board-main-logo' alt='Main-icon' src={logo}/>
+                            <button className='Board-main-button' onClick={this.newJokes}>New <span>Jokes</span></button>
+                        </div>
+                        <div className='Board-jokes'>
+                            {this.state.jokes.map(joke => {
+                                    return <Jokes 
+                                            key={joke.id} 
+                                            id={joke.id} 
+                                            joke={joke.joke} 
+                                            vote={joke.vote} 
+                                            upVote={this.upVote} 
+                                            downVote ={this.downVote}                                   
+                                            />
+                                })
+                            }
+                        </div>
+                    </div> )
+                : (<div className='loader'></div>)
+                }
             </div> 
         );
     }
---
+++ src/Jokes.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import './Jokes.css';
+import laugh1 from './laugh1.png';
+import laugh2 from './laugh2.png';
+import laugh3 from './laugh3.png';
+import normal from './normal.png';
+import sad from './sad.png';
+
+class Jokes extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+
+    iconDisplay() {
+        if (this.props.vote < 0){
+            return sad;
+        } else if (this.props.vote > 10) {
+            return laugh3;
+        } else if (this.props.vote > 5){
+            return laugh2;
+        } else if (this.props.vote > 0){
+            return laugh1;
+        } else {
+            return normal;
+        };
+    }
+    
+    render() { 
+        // const icon = this.iconDisplay()
+        return ( 
+            <div className='Jokes'>
+                <div className='Jokes-score'>
+                    <span className='Jokes-score-up' onClick={() => this.props.upVote(this.props.id)}><i className="fas fa-arrow-up"></i></span>
+                    <span className='Jokes-vote'>
+                       {this.props.vote}
+                    </span>
+                    <span className='Jokes-score-down' onClick={() => this.props.downVote(this.props.id)}><i className="fas fa-arrow-down"></i></span> 
+                </div>
+                <div className='Jokes-display'>
+                    <p className='Jokes-description'>{this.props.joke}</p>
+                </div>
+                <div className='Jokes-icon'>
+                    <img className='Jokes-icon-image' alt='emotion' src={this.iconDisplay()}/>
+                </div>
+            </div> 
+        );
+    }
+}
+ 
+export default Jokes;
